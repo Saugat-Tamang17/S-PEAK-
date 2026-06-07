224d1ce0b741669af5356d094d58bb462a764493
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-07 18:46:05 +0545

    loaded and connected env file credentials with config

--- config/config.go
+++ config/config.go
@@ -2,10 +2,36 @@
 
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	DBHOST     string //HOST IP ADDRESS //
 	DBPORT     string //ON WHICH PORT ARE WE HOSTING IT//
 	DBUSER     string //NAME OF DATABSE USER//
 	DBPASSWORD string //PASSWORD OF DB//
 	DBNAME     string //NAME OF DB//
+
+	//GROQ WILL COVER BOTH WHISPER STT AND LLM //
+	GROQAPIKEY string
+	PORT       string
+}
+
+func load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No env file found,reading env files directly")
+	}
+	cfg := &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		GroqAPIKey: os.Getenv("GROQAPIKEY"),
+		Port:       os.Getenv("PORT"),
+	}
 }

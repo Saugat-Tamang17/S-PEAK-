a35cefdb8b5981242f0dce309653cdae39d9456b
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-07 19:12:14 +0545

    checking if the config loaded successfully or not for this day1

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Saugat-Tamang17/S-PEAK/config"
+)
+
+func main() {
+	cfg := config.Load()
+
+	//day1:CHECKING IF the config loaded correctly just for now
+	fmt.Println("Config loaded sucessfully.")
+	fmt.Printf("DB Host : %s\n", cfg.DBHOST)
+	fmt.Printf("DB Port : %s\n", cfg.DBPORT)
+	fmt.Printf("DB Name : %s\n", cfg.DBNAME)
+	fmt.Printf("PORT : %s\n", cfg.PORT)
+}

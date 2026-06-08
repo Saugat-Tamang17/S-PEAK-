9b49d451a8e76ef93c6d04cf0275e868b9c1bce1
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-08 19:32:57 +0545

    added a small error handling part which readies up the driver for the mysql connection

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -2,8 +2,16 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 func Connect(host, port, user, password, name string) (*sql.DB, error) {
-
+ dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+        user, password, host, port, name,
+    )
+		db,err:=sql.Open("mysql",dsn);
+		if err!=nil{
+			return nil,fmt.Errorf("
+				failed to open database:%w",err)
+		}
 }

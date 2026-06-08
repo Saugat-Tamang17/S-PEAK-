48daa418ac6735e6753d91f64568f9bdcc9308e9
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-08 19:35:01 +0545

    added the pinging error handler as well which ensures the connection of our api with db

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -6,12 +6,15 @@ import (
 )
 
 func Connect(host, port, user, password, name string) (*sql.DB, error) {
- dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-        user, password, host, port, name,
-    )
-		db,err:=sql.Open("mysql",dsn);
-		if err!=nil{
-			return nil,fmt.Errorf("
-				failed to open database:%w",err)
-		}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		user, password, host, port, name,
+	)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database:%w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to establish a connection with db (cant ping db as of now ):%w", err)
+	}
+
 }

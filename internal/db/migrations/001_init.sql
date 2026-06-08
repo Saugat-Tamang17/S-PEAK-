28208d2e7719888336a75229833356112843b847
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-08 19:08:19 +0545

    fixed the mistake where user_id was misreferenced earlier from int to varchar

--- internal/db/migrations/001_init.sql
+++ internal/db/migrations/001_init.sql
@@ -1,5 +1,6 @@
-create table if not exist users(
-id int AUTO_INCREMENT PRIMARY KEY, 
+-- Active: 1780405368569@@127.0.0.1@3306@s_peak
+create table if not exists users(
+  id int AUTO_INCREMENT PRIMARY KEY, 
   email varchar(255) not null unique,
   password_hash varchar(255) not null,
   created_at TIMESTAMP default CURRENT_TIMESTAMP
@@ -7,8 +8,8 @@ id int AUTO_INCREMENT PRIMARY KEY,
 
 create table if not exists sessions(
   id int AUTO_INCREMENT PRIMARY KEY,
-  user_id varchar(100) not null,
+  user_id int not null,
   mode varchar (20) not null,
-  created_at timestamp default CURRENT_TIMESTAMP
+  created_at timestamp default CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id)
 )

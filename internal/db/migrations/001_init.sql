4fc410124eb6e43ecb916b6b245682865ff66b9e
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-08 18:56:50 +0545

    initial schema or blueprint for the sessions ( mode selection )

--- internal/db/migrations/001_init.sql
+++ internal/db/migrations/001_init.sql
@@ -5,3 +5,10 @@ id int AUTO_INCREMENT PRIMARY KEY,
   created_at TIMESTAMP default CURRENT_TIMESTAMP
 );
 
+create table if not exists sessions(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  user_id varchar(100) not null,
+  mode varchar (20) not null,
+  created_at timestamp default CURRENT_TIMESTAMP
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)

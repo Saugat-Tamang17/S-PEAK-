68abaa8c6a46e9db3d8ed048508b7f1b55bfc1e1
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-09 18:45:39 +0545

    Implementation : using r.Use() implies middleware in all routes with logger and recoverer doing own task

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,8 +2,12 @@ package api
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter() *chi.Mux {
+	r := chi.NewRouter()
 
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 }

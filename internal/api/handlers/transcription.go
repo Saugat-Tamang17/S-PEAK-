ffff34cd00af29a40f016066a0ffb7b944262643
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-09 19:13:51 +0545

    minimal version of the transcription.go that sends the data in json hard coded string for now

--- internal/api/handlers/transcription.go
+++ internal/api/handlers/transcription.go
@@ -1,1 +1,13 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func TranscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"Message": "Transcription endpoint - new features on the way",
+	})
+}

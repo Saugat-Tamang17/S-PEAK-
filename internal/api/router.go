fcac33d068b2b5fa585cf7d7ff56183df1d24f9d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-09 18:49:20 +0545

    added CORS middleware to routers

--- internal/api/router.go
+++ internal/api/router.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter() *chi.Mux {
@@ -10,4 +11,10 @@ func NewRouter() *chi.Mux {
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"} // meaning any websites can make http req to my api as of now ,//
+		AllowedMethods: []string{"GET","POST","OPTIONS"},
+		AllowedHeaders: :[]string{"Content-Type"},
+	}))
 }

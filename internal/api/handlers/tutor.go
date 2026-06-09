52820e262895fe05a1a2020e459bb6ec77e724b1
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-09 19:20:02 +0545

    minimal version of the tutor.go

--- internal/api/handlers/tutor.go
+++ internal/api/handlers/tutor.go
@@ -1,1 +1,13 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func TutorHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"Message": "Tutor endpoint - new features on the way",
+	})
+}

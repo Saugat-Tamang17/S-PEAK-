3c5b9c8fbab7024e1f803b48742fa5178fbcfbc9
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-09 19:04:51 +0545

    completed the Health Handler which returns the healthy connection and condition of the api by sending http.StatusOK in the json

--- internal/api/handlers/health.go
+++ internal/api/handlers/health.go
@@ -1,9 +1,15 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"Status": "ok",
+		"app":    "S-PEAK",
+	})
 }

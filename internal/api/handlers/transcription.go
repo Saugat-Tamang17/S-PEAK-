8b2ed67ee0c1c63c1916cdfde72f0840ea645d70
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-10 20:59:41 +0545

    contri farming

--- internal/api/handlers/transcription.go
+++ internal/api/handlers/transcription.go
@@ -8,5 +8,10 @@ func TranscribeHandler(apiKey string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		r.ParseMultipartForm(10 << 20) //meaning 10 MB max for audio form//
 
+		file, header, err := r.FormFile(("audio"))
+		if err != nil {
+			http.Error(w, "audio file required", http.StatusBadRequest)
+			return
+		}
 	}
 }

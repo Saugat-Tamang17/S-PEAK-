ba6454c0f6b6ac319ca01d194c35f581bf4b4994
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-10 21:10:21 +0545

    completed the transcribe feature to some extent , will need testing

--- internal/api/handlers/transcription.go
+++ internal/api/handlers/transcription.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+
+	"github.com/Saugat-Tamang17/S-PEAK/internal/services/groq"
 )
 
 func TranscribeHandler(apiKey string) http.HandlerFunc {
@@ -13,5 +17,18 @@ func TranscribeHandler(apiKey string) http.HandlerFunc {
 			http.Error(w, "audio file required", http.StatusBadRequest)
 			return
 		}
+		defer file.Close()
+
+		audioBytes, _ := io.ReadAll(file)
+
+		transcript, err := groq.Transcribe(apiKey, audioBytes, header.Filename)
+		if err != nil {
+			http.Error(w, "transcription failed", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"transcript": transcript,
+		})
 	}
 }

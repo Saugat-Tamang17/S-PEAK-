9cb003ac4e6a2f6e2a0e0004a1437cd80c38033c
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-11 19:21:38 +0545

    created a multipart/form data containing audio file and model name

--- internal/services/groq/transcribe.go
+++ internal/services/groq/transcribe.go
@@ -1,7 +1,26 @@
 package groq
 
+import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+)
+
 const transcribeURL = "https://api.groq.com/openai/v1/audio/transcriptions"
 
 type TranscribeResponse struct {
 	Text string `json:text`
+}
+
+func transcribe(apiKey string, audioData []byte, filename string) (string, error) {
+	var body bytes.Buffer
+	writer := multipart.NewWriter(&body)
+	part, err := writer.CreateFormFile("file", filename)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create form file:%w", err)
+	}
+
+	part.Write(audioData)
+	writer.WriteField("model", "whisper-large-v3")
+	writer.Close()
 }

cd7f49b6261a44b25a6b057e92c67965d7f8c7ba
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-15 17:38:13 +0545

    unmarshalling the data (transcript raw ) we are sending to AI for better transport

--- internal/services/groq/enhancer.go
+++ internal/services/groq/enhancer.go
@@ -1,24 +1,35 @@
 package groq
 
-func Enhance(apiKey string, rawTranscript string) (string, error) {
-payload :=map[string]interface{}{ //this is used for json like structure //
+import (
+	"encoding/json"
+	"fmt"
+)
 
-	"model":"llama-3.3-70b-versatile",
-	"messages":[]map[string]string{
-		"role":"system",
-		"content":`You are a speech transcription editor.Clean up the following transcript by:
+func Enhance(apiKey string, rawTranscript string) (string, error) {
+	payload := map[string]interface{}{ // this is used for json-like structure
+		"model": "llama-3.3-70b-versatile",
+		"messages": []map[string]string{
+			{
+				"role": "system",
+				"content": `You are a speech transcription editor. Clean up the following transcript by:
 - Removing filler words (um, uh, like, you know)
 - Fixing obvious grammar errors
 - Breaking run-on sentences
-- Preserving the speaker's original meaning and vocabulary.Return onlly the cleaned transcripted text.No commentary,no preamble. `,
-	},
-	{
-      "role":    "user",
-      "content": rawTranscript,
-    },
-		"max_tokens":1024, //approximate around 750 words per session //
+- Preserving the speaker's original meaning and vocabulary. Return only the cleaned transcribed text. No commentary, no preamble.`,
+			},
+			{
+				"role":    "user",
+				"content": rawTranscript,
+			},
+		},
+		"max_tokens":  1024, // approximate around 750 words per session
+		"temperature": 0.3,  // low temperature = more conservative edits of transcripts
+	}
 
-"temperature":0.3, // low temperature =more conservative edits of transcripts
-},
- 
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return "", fmt.Errorf("Failed to marshal the payload :%w", err)
+	}
+
+	return string(body), nil
 }

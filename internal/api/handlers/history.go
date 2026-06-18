7526d9c7505678fe4af946fc907777c3b110117d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-18 19:03:22 +0545

    func for the history handler which extracts the sessionRow

--- internal/api/handlers/history.go
+++ internal/api/handlers/history.go
@@ -3,10 +3,17 @@ package handlers
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/Saugat-Tamang17/S-PEAK/internal/db"
 )
 
 func HistoryHandler(database *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		//hardcoded user =1 ,until auth is wired later on
+		rows, err := db.GetSessionHistory(database, 1)
+		if err != nil {
+			http.Error(w, "failed to fetch history", http.StatusInternalServerError)
+			return
+		}
 	}
 }

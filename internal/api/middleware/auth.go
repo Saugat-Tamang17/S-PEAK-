20dedbdcecc098e448027a7446f938e2f128f7fa
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-22 19:10:15 +0545

    implement JWT Bearer token validation middleware

--- internal/api/middleware/auth.go
+++ internal/api/middleware/auth.go
@@ -1,6 +1,19 @@
 package middleware
 
-//not using stringdirectly to avoid context key collision //
+import (
+	"net/http"
+)
+
+// not using stringdirectly to avoid context key collision //
 type contextKey string
 
 const UserIDKey contextKey = "user_id"
+
+func JWTAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if !string.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "missing or malfunctioned tokens", http.StatusUnauthorized)
+		}
+	})
+}

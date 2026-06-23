28bca808bd50cc77442cd85a45bec9245087f7b8
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-23 18:48:28 +0545

    nvm now puttin it at the top and redoing the work again

--- internal/api/router.go
+++ internal/api/router.go
@@ -30,58 +30,16 @@ func NewRouter(cfg *config.Config, database *sql.DB) *chi.Mux {
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
-	  authHandler := &handlers.AuthHandler{DB: db}
-	r.Post("/api/v1/auth/register", authHandler.Register)
-	r.Post("/api/v1/auth/login", authHandler.Login)
-
-	
-
-
-
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"*"}, // meaning any websites can make http req to my api as of now ,//
 		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders: []string{"Content-Type"},
 	}))
-
-	r.Get("/health", handlers.HealthHandler)
-  r.Group(func(r chi.Router) {
-        r.Use(middleware.JWTAuth)
-
-        r.Post("/api/v1/tutor", handlers.TutorHandler(cfg,db))
-        r.Post("/api/v1/transcription", handlers.TranscribeHandler(cfg,db))
-        r.Get("/api/v1/history", handlers.HistoryHandler(db))
-    })
-
-    return r
-}
-
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	  authHandler := &handlers.AuthHandler{DB: db}
-	r.Post("/api/v1/auth/register", authHandler.Register)
-	r.Post("/api/v1/auth/login", authHandler.Login)
-
+	 
 	
 
 
 
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"*"}, // meaning any websites can make http req to my api as of now ,//
-		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
-		AllowedHeaders: []string{"Content-Type"},
-	}))
 
-	r.Get("/health", handlers.HealthHandler)
-  r.Group(func(r chi.Router) {
-        r.Use(middleware.JWTAuth)
-
-        r.Post("/api/v1/tutor", handlers.TutorHandler(cfg,db))
-        r.Post("/api/v1/transcription", handlers.TranscribeHandler(cfg,db))
-        r.Get("/api/v1/history", handlers.HistoryHandler(db))
-    })
-
-    return r
+return r
 }

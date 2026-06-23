1dfca8acdb40d7678c7943d9a22d52a254d0573d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-06-23 18:46:55 +0545

    fix : CORS was applied to late so changing it to top so that it protects all routes

--- internal/api/router.go
+++ internal/api/router.go
@@ -12,13 +12,32 @@ import (
 
 func NewRouter(cfg *config.Config, database *sql.DB) *chi.Mux {
 	r := chi.NewRouter()
+package api
+
+import (
+	"database/sql"
+
+	"github.com/Saugat-Tamang17/S-PEAK/config"
+	"github.com/Saugat-Tamang17/S-PEAK/internal/api/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func NewRouter(cfg *config.Config, database *sql.DB) *chi.Mux {
+	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	authHandler := &handlers.AuthHandler{DB: db}
+	  authHandler := &handlers.AuthHandler{DB: db}
 	r.Post("/api/v1/auth/register", authHandler.Register)
 	r.Post("/api/v1/auth/login", authHandler.Login)
+
+	
+
+
 
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"*"}, // meaning any websites can make http req to my api as of now ,//
@@ -27,13 +46,42 @@ func NewRouter(cfg *config.Config, database *sql.DB) *chi.Mux {
 	}))
 
 	r.Get("/health", handlers.HealthHandler)
-	r.Group(func(r chi.Router) {
-		r.Use(middleware.JWTAuth)
+  r.Group(func(r chi.Router) {
+        r.Use(middleware.JWTAuth)
 
-		r.Post("/api/v1/tutor", handlers.TutorHandler(cfg, db))
-		r.Post("/api/v1/transcription", handlers.TranscribeHandler(cfg, db))
-		r.Get("/api/v1/history", handlers.HistoryHandler(db))
-	})
+        r.Post("/api/v1/tutor", handlers.TutorHandler(cfg,db))
+        r.Post("/api/v1/transcription", handlers.TranscribeHandler(cfg,db))
+        r.Get("/api/v1/history", handlers.HistoryHandler(db))
+    })
 
-	return r
+    return r
+}
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	  authHandler := &handlers.AuthHandler{DB: db}
+	r.Post("/api/v1/auth/register", authHandler.Register)
+	r.Post("/api/v1/auth/login", authHandler.Login)
+
+	
+
+
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"}, // meaning any websites can make http req to my api as of now ,//
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	}))
+
+	r.Get("/health", handlers.HealthHandler)
+  r.Group(func(r chi.Router) {
+        r.Use(middleware.JWTAuth)
+
+        r.Post("/api/v1/tutor", handlers.TutorHandler(cfg,db))
+        r.Post("/api/v1/transcription", handlers.TranscribeHandler(cfg,db))
+        r.Get("/api/v1/history", handlers.HistoryHandler(db))
+    })
+
+    return r
 }

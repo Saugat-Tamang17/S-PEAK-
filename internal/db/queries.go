94da60a49a303cfbab45cd3239e25079077e260a
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-14 19:18:54 +0545

    added the name arguement as the parameter in the CreateUser func signature

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -14,7 +14,7 @@ type User struct {
 	GoogleID     string
 }
 
-func CreateUser(ctx context.Context, db *sql.DB, email, hashedPassword string) error {
+func CreateUser(ctx context.Context, db *sql.DB, name, email, hashedPassword string) error {
 	_, err := db.ExecContext(ctx, "INSERT INTO users (email, password_hash) VALUES(?, ?)", email, hashedPassword)
 	if err != nil {
 		return err

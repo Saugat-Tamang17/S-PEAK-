01b4ddf5e49ace7156dc025fa9b55d4a1aaee0ab
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-20 13:20:29 +0545

    getClientIp will track the exact device or IP that sent the req so that our rate limiter will track which user to track and limit

--- internal/api/middleware/ratelimit.go
+++ internal/api/middleware/ratelimit.go
@@ -1,10 +1,15 @@
 package middleware
 
 import (
+	"net/http"
 	"time"
 )
 
 type visitor struct {
 	count     int       //how many request the ip has made so far
 	windowEnd time.Time // meaning how much req were made , during that time window ( for eg: 60 req in 1 min)//
+}
+
+func getClientIP(r *http.Request, trustProxyHeaders bool) string {
+
 }

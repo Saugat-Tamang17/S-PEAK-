ad45e46171d063b12811f40c6f082cacaac1652c
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-20 13:11:58 +0545

    added struct that defines the req count and the frequency of the req per time

--- internal/api/middleware/ratelimit.go
+++ internal/api/middleware/ratelimit.go
@@ -1,1 +1,10 @@
 package middleware
+
+import (
+	"time"
+)
+
+type visitor struct {
+	count     int       //how many request the ip has made so far
+	windowEnd time.Time // meaning how much req were made , during that time window ( for eg: 60 req in 1 min)//
+}

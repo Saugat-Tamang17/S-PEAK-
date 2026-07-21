d27c05abb1801138c95bf88a30a45fef759680c5
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-21 18:52:46 +0545

    add the golang struct for parsing the json web key (jwk ) issued by google

--- internal/api/handlers/google.go
+++ internal/api/handlers/google.go
@@ -4,3 +4,9 @@ import {
 }
 
 const googleJWKSURL = "https://www.googleapis.com/oauth2/v3/certs"
+
+type googleJWK struct {
+	Kid string `json:"kid"` //keyID pr uniqwue token for the header
+	N   string `json:"n"` //modulus(n)
+	E   string `json:"e"` //exponent(e)
+}

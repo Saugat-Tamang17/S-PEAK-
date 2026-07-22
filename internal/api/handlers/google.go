f75a53a96a60f367c788a3e6a2ca57c514708b14
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-22 18:18:05 +0545

    added the function which guard and request the validatio pahse of google authentication endpoint

--- internal/api/handlers/google.go
+++ internal/api/handlers/google.go
@@ -1,5 +1,16 @@
 package handlers
 
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/Saugat-Tamang17/S-PEAK/config"
+)
+
 type googleAuthRequest struct {
 	IDtoken string `json:"id_token"`
+}
+
+func GoogleAuthHandler(cfg *config.Config, database *sql.DB) http.HandlerFunc {
+
 }

dd3584f638f3b87e396935b16309eab6c7cfca25
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-22 18:20:16 +0545

    added a clause that checks if the googleClientID ,sent from the frontend is empty or not to the backend

--- internal/api/handlers/google.go
+++ internal/api/handlers/google.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/Saugat-Tamang17/S-PEAK/config"
@@ -12,5 +13,9 @@ type googleAuthRequest struct {
 }
 
 func GoogleAuthHandler(cfg *config.Config, database *sql.DB) http.HandlerFunc {
-
+	return func(w http.ResponseWriter, r *http.Request) {
+		if cfg.GoogleClientID == "" {
+			log.Println("google auth attempted but GOOGLECLIENT_ID isnt set")
+		}
+	}
 }

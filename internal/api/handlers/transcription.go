5cda5110339be4cd73a360d34140f0a3d530d451
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-28 17:56:54 +0545

    stopping attributing to every user's transcripts

--- internal/api/handlers/transcription.go
+++ internal/api/handlers/transcription.go
@@ -8,12 +8,19 @@ import (
 	"net/http"
 
 	"github.com/Saugat-Tamang17/S-PEAK/config"
+	"github.com/Saugat-Tamang17/S-PEAK/internal/api/middleware"
 	"github.com/Saugat-Tamang17/S-PEAK/internal/db"
 	groq "github.com/Saugat-Tamang17/S-PEAK/internal/services/groq"
 )
 
 func TranscribeHandler(cfg *config.Config, database *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		userID, ok := r.Context().Value(middleware.UserIDKey).(int)
+		if !ok {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
 		err := r.ParseMultipartForm(10 << 20)
 		if err != nil {
 			log.Printf("ParseMultipartForm error: %v", err)
@@ -41,11 +48,10 @@ func TranscribeHandler(cfg *config.Config, database *sql.DB) http.HandlerFunc {
 		enhanced, err := groq.Enhance(cfg, rawtranscript)
 		if err != nil {
 			log.Printf("Enhance error: %v — falling back to raw", err)
-			enhanced = rawtranscript // degrade gracefully, don't fail the whole request
+			enhanced = rawtranscript
 		}
-		//save to DB
-		// Hardcoded userID=1 for now — auth comes in Week 3
-		sessionID, err := db.CreateSession(database, 1, "transcription")
+
+		sessionID, err := db.CreateSession(database, userID, "transcription")
 		if err != nil {
 			log.Printf("DB session error: %v", err)
 			http.Error(w, "database error", http.StatusInternalServerError)

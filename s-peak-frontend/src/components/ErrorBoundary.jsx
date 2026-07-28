b81d54fb011ec17ba37de688c2b4f3f46c6931a7
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-28 18:25:28 +0545

    implemented getDerivedStateFromError in errorboundary

--- s-peak-frontend/src/components/ErrorBoundary.jsx
+++ s-peak-frontend/src/components/ErrorBoundary.jsx
@@ -4,6 +4,10 @@ export default class ErrorBoundary extends React.Components{
   consturctor(pops){
     super(props);
     this.state={hasError:false};
-    
+
+  }
+
+  static getDerivedStateFromError(){
+    return {hasError:true};
   }
 }

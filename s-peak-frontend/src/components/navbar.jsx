b953e987a4867b44aa88a36373a8f02a583edc74
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-29 18:12:51 +0545

    added the handleout function and redirect user to login

--- s-peak-frontend/src/components/navbar.jsx
+++ s-peak-frontend/src/components/navbar.jsx
@@ -1,3 +1,13 @@
 import React from "react";
 import {Link, useNavigate,useLocation }from "react-router-dom";
 import { clearToken} from "../lib/api"
+
+export default function Navbar(){
+  const navigate=useNavigate()
+  const location=useLocation()
+}
+
+const handleLogout =()=>{
+  clearToken();
+  navigate("/login");
+}

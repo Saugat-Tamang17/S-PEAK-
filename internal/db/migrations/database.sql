3901b48754f4480bf169e5bc83cb43ff7070274b
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-03 18:42:00 +0545

    removed the google id for the user so that it doesnt create the reducdency with the google_oauth

--- internal/db/migrations/database.sql
+++ internal/db/migrations/database.sql
@@ -15,7 +15,7 @@ CREATE TABLE users (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     email         VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NULL,
-    google_id     VARCHAR(255) NULL UNIQUE,
+   
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
